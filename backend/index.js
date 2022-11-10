67c4f6a859a353296c304b310d43df53b134d126
HarpratapsinhNakoom <ui20cs26@iiitsurat.ac.in>
2022-11-10 20:26:47 +0530

    Connected database

--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,22 @@
+const mongoose = require("mongoose")
+
 const express = require("express")
 const app = express()
+
+
 
 const dotenv = require("dotenv")
 dotenv.config();
 
 const PORT = process.env.PORT || 5500
+
+mongoose.connect(
+    process.env.MONGODB_URL
+).then(() => {
+    console.log('DB connected')
+}).catch((err) => {
+    console.log(err);
+})
 
 app.listen(PORT, () => {
     console.log("Server listening on PORT - ",PORT);
