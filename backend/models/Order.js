ad530dc2627215e590d71de501c2943db8118699
HarpratapsinhNakoom <ui20cs26@iiitsurat.ac.in>
2022-11-10 20:42:22 +0530

    Added Order schema
    Order schema has
    email
    array of [productID, price, quantity]
    total amaount
    address
    status

---
+++ backend/models/Order.js
@@ -0,0 +1,26 @@
+const mongoose = require("mongoose");
+
+
+//----Making Order schema model for database----
+
+const orderSchema = new mongoose.Schema(
+    {
+        email : {type : String, required : true},
+        productList : [
+           {
+                productID : {type : String},
+                price : {type:Number},
+                quantity : {type : Number, default : 1},
+           }
+        ],
+        amount : {type:Number, required : true},
+        userAddress : {type:Object, required : true},
+        status : {type:String, default : "pending"}
+    },
+    {
+        timestamps:true
+        //--will automatically do created and updated times
+    }
+)
+
+module.exports = mongoose.model("Order", orderSchema);
