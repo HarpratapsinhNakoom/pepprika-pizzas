6e1db1706295ee5188a37f1482decc578ae29d16
HarpratapsinhNakoom <ui20cs26@iiitsurat.ac.in>
2022-11-13 19:02:35 +0530

    Added top picks section
    Need to change information in array about different pizzas

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import Banner from "../components/Banner";
 import Menu from '../components/Menu';
+import TopPicks from '../components/TopPicks';
 
 function Home() {
     return (
         <>
             <Banner />
             <Menu />
+            <TopPicks />
         </>
     )
 }
