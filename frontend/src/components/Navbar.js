57b3a6ea08925389380c91d162f0c7a3ed03540d
HarpratapsinhNakoom <ui20cs26@iiitsurat.ac.in>
2022-11-15 22:39:50 +0530

    Modified navbar background

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -5,27 +5,25 @@ import { CiLinkedin } from 'react-icons/ci'
 
 function NavbarComponent() {
     return (
-        <div className="navbar-bg">
-            <div className='navbar-container'>
-                <div className="navbar-left">
-                    <div className="navbar-title">𝒫𝑒𝓅𝓅𝓇𝒾𝓀𝒶 𝒫𝒾𝓏𝓏𝒶𝓈</div>
+        <div className='navbar-container'>
+            <div className="navbar-left">
+                <div className="navbar-title">𝒫𝑒𝓅𝓅𝓇𝒾𝓀𝒶 𝒫𝒾𝓏𝓏𝒶𝓈</div>
+            </div>
+            <div className="navbar-center">
+                <div className="navbar-center-side">
+                    <p>Our Menu</p>
+                    <p>About</p>
                 </div>
-                <div className="navbar-center">
-                    <div className="navbar-center-side">
-                        <p>Our Menu</p>
-                        <p>About</p>
-                    </div>
-                    <div className="navbar-center-side">
-                        <p>Login</p>
-                        <p>Signup</p>
-                        <p><BsCart /></p>
-                    </div>
+                <div className="navbar-center-side">
+                    <p>Login</p>
+                    <p>Signup</p>
+                    <p><BsCart /></p>
                 </div>
-                <div className="navbar-right">
-                    <BsGithub />
-                    <CiLinkedin />
-                    <BsInstagram />
-                </div>
+            </div>
+            <div className="navbar-right">
+                <BsGithub />
+                <CiLinkedin />
+                <BsInstagram />
             </div>
         </div>
     );
