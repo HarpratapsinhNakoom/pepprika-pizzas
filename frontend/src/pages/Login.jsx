4b39be6389dc37e97e5da21c52d72ad4d8d3be6e
HarpratapsinhNakoom <ui20cs26@iiitsurat.ac.in>
2022-11-16 11:00:35 +0530

    Updated login page
    Added functionality for user to select if he is a customer or an admin

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,7 +1,21 @@
 import React from 'react'
 import '../styles/Login.css'
+import { Link } from 'react-router-dom'
+import {RiAdminFill} from 'react-icons/ri'
+import {AiOutlineShopping} from 'react-icons/ai'
 
 const Login = () => {
+  const [userType, setUserType] = React.useState("customer");
+
+  const handleUserType = () => {
+      if (userType === "customer") {
+          setUserType("admin");
+      }else {
+        setUserType("customer");
+      }
+
+
+  }
   return (
     <div className='login-container'>
         <div className='login-image-container'>
@@ -9,6 +23,20 @@ const Login = () => {
         </div>
         <div className="login-box">
             <h1 className="login-title">Log In</h1>
+            <div className="customer-admin">
+                <div
+                  className={`icon-part ${userType === "customer" ? "selected" : ""}`}
+                  onClick={handleUserType}>
+                    <RiAdminFill className='login-icons'/>
+                    <p>Customer</p>
+                </div>
+                <div 
+                  className={`icon-part ${userType === "admin" ? "selected" : ""}`}
+                  onClick = {handleUserType}>
+                    <AiOutlineShopping className='login-icons'/>
+                    <p>Admin</p>
+                </div>
+            </div>
             <div className="input-elements">
                 <label htmlFor="email">Email address</label>
                 <input type="email" id="email" name='email'/>
@@ -16,6 +44,7 @@ const Login = () => {
                 <input type="password" name="password" id='password' />
             </div>
             <button className='submit-btn btn'>Log in</button>
+            <p>Don't have an account? <Link to="/signup">Sign up</Link> </p>
         </div>
     </div>
   )
