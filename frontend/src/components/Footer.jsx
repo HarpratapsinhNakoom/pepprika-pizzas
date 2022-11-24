1e89b7a3c1f1e52b71281609553155924f938ceb
IamHarsh05 <harshchaudhari592@gmail.com>
2022-11-24 15:17:05 +0530

    change

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 import React from 'react';
 import "../styles/Footer.css";
 import { IconName } from "react-icons/ai";
@@ -93,3 +94,100 @@ export default function Footer(){
 }
 
 
+=======
+import React from 'react';
+import "./Footer.css";
+import { IconName } from "react-icons/ai";
+
+export default function Footer(){
+  return (  
+    <footer className="footer">
+        <div className="row">
+            <div className="section-heading">
+                <div className="bar">
+                <h2>Useful Links</h2>
+                    <span className="animate-border border-black"></span>                
+                <ul>
+                    <li>
+                        <a>Menu</a>
+                    </li>
+                    <li>
+                        <a>Catering</a>
+                    </li>
+                    <li>
+                        <a>Press</a>
+                    </li>
+                    <li>
+                        <a>Careers</a>
+                    </li>
+                    <li>
+                        <a>Rewards</a>
+                    </li>
+                    <li>
+                        <a>Franchise</a>
+                    </li>                                        
+                    <li>
+                        <a>Order Now</a>
+                    </li>
+                </ul>
+                </div>
+            </div>
+            <div className="timing">
+                <div className="h3">
+                    <h2>Working Hours</h2>
+                    <span className="animate-border border-black"></span>
+                </div>                    
+                    <p className="day">Sunday-Thursday</p>
+                    <p>11:00 AM TO 9:00 PM</p>
+                    <p className="day">FRIDAY-SATURDAY</p>
+                    <p>11:00 AM TO 10:00 PM</p>
+            </div>
+            <div className="social">
+                <ul>
+                    <li>
+                        <a href='#' className="fa-facebook"></a>
+                    </li>
+                    <li>
+                        <a href='#' className="fa-twitter"></a>
+                    </li>
+                    <li>
+                        <a href='#' className="fa-google-plus"></a>
+                    </li>
+                    <li>
+                        <a href='#' className="fa-linkedin"></a>
+                    </li>
+                    <li>
+                        <a href='#' className="fa-instagram"></a>
+                    </li>
+                </ul>                                    
+            </div>                                            
+            <div className="join">
+                <h3>JOIN OUR NEWSLETTER!</h3>
+                <span className="animate-border border-black"></span>         
+              <div className="form-row">                        
+                    <input type="email" className="form-control" placeholder="xyz@email"></input>
+                    <a href="" className="but">&#160;&#62;&#62;</a>                                                 
+              </div>
+            </div>                                         
+            <div id="back-to-top" className="back-to-top">
+            </div>
+            <div className="call">
+                <h2>Call Us At</h2>
+                <span className="animate-border border-black"></span> 
+                <ul>
+                    <li><a>Privacy Policy</a></li>
+                    <li><a>Legal Diclaimer</a></li>
+                    <li><a>Term of Use</a></li>
+                    <li><a>Join Our Team</a></li>
+                </ul>
+            </div>
+            <div className="copy">
+                <p>&#174; 2022 Pepprika Pizza Co. All Rights</p>
+            </div>
+        </div>
+    </footer> 
+  )  
+}
+
+
+>>>>>>> Stashed changes
